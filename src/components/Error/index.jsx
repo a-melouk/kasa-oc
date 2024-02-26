81ec0ddf98bd0bf740e9956b79ef8a2c3b5986ab
mabdou <abdoumelouk96@gmail.com>
2024-02-26 10:38:42 +0100

    Created Normalize file to normalize default css styles

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,27 @@
 import { NavLink } from "react-router-dom";
 import "../../styles/Error.scss";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function Error() {
+  const location = useLocation();
+  // Check if the current route is the root page ("/")
+  const isRootPage = location.pathname === "/";
+
+  // Conditionally apply the class to the root element
+  useEffect(() => {
+    const rootElement = document.getElementById("root");
+    if (isRootPage) {
+      rootElement.classList.add("no-gap");
+    } else {
+      rootElement.classList.remove("no-gap");
+    }
+
+    // Clean up the class when the component unmounts
+    return () => {
+      rootElement.classList.remove("no-gap");
+    };
+  }, [isRootPage]);
   return (
     <div className="error">
       <h1 className="error__heading">404</h1>
