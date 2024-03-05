296a2a210e426fabab6d4c2f5031c4f71a406bdc
mabdou <abdoumelouk96@gmail.com>
2024-03-05 10:23:14 +0100

    Added card cover instead of orange background

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,6 +4,7 @@ function Card({ logement }) {
   return (
     <Link to={`/logement/${logement.id}`}>
       <div className="thumb">
+        <img src={logement.cover} alt={logement.title} className="thumb__img" />
         <h2 className="thumb__title">{logement.title}</h2>
       </div>
     </Link>
